934c0a23e2d0feaeafee2f49958093f50d238afc
shwetankrai12 <shwetankrai12102004@gmail.com>
2025-06-25 12:49:17 +0530

    day 1 of learning Cpp

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CPP-DSA-daily-progress
 I am learning and want to show my works to other people through this platform.
+- Day 1 (25 jun,2025) - I learned about basic data types, and wrote my first program.
---
+++ day_1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+usinq namespace std;
+int main() {
+    cout << "Hello, World!" << endl;
+    int number = 42;
+    cout << "The number is: " << number << endl;
+    char character = 'A';
+    cout << "The character is: " << character << endl;
+    double pi = 3.14159;
+    cout << "The value of pi is: " << pi << endl;
+    bool isTrue = true;
+    cout << "Is it true? " << (isTrue ? "Yes" : "No") << endl;
+    string message = "Welcome to C++ programming!";
+    cout << message << endl;
+    return 0;
+}
