cf979abfcd86e4bbaf208ac2713ab1d5ad4dd7f1
shwetankrai12 <shwetankrai12102004@gmail.com>
2025-08-18 10:41:22 +0530

    day 11, sqrt upto 3 precision digits

--- Daily log/day_10.cpp
+++ Daily log/day_10.cpp
@@ -1,3 +1,5 @@
+// code for finding the pivot in a rotated sorted array
+ 
 #include <iostream>
 
 using namespace std;
@@ -29,4 +31,57 @@ int main () {
     int arr [5] = {7,9,1,2,3};
 
     cout << "the pivot is at index: " << getpivot (arr,5) << endl;
+} 
+
+// code for finding sqrt upto 3 demimal places
+#include <iostream>
+using namespace std;
+int sqrtInt(int n) {
+
+    int s = 0;
+    int e = n/2;
+    int ans = -1;
+    if (n==1) {
+        return 1;
+    }
+    while (s <= e) {
+
+      long long mid = s + (e - s)/2;
+
+      if (mid*mid == n) {
+
+        return mid;
+
+      }
+      else if (mid*mid < n) {
+        ans = mid;
+        s = mid +1;
+    } else {
+        e = mid - 1;
+
+    }
+    }
+return ans;
+}
+double sqrtdecimal (int n, int precision, int tempsol) {
+
+    double factor = 1;
+    double ans = tempsol;
+    for (int i = 0; i < precision; i++) {
+
+        factor = factor /10;
+        for (double j = ans ; j*j <= n; j= j + factor) {
+            ans = j;
+        }
+
+    }
+    return ans;
+}
+int main () {
+
+    int n;
+    cout << "Enter the number to find the square root: ";
+    cin >> n;
+    int ans = sqrtInt(n);
+    cout << "The integer part of the square root is: " << sqrtdecimal(n,3,ans) << endl;
 }
